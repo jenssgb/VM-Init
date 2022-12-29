6dc1e762be6f0cdfd6affd3312611df683cde809
jenssgb <jens.epost@googlemail.com>
2022-12-29 22:39:32 +0100

    Added First Script for UI Changes

---
+++ configer-ui.ps1
@@ -0,0 +1,7 @@
+#Switch to Black Mode 
+Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name AppsUseLightTheme -Value 0 -Type Dword -Force
+Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name SystemUsesLightTheme -Value 0 -Type Dword -Force
+
+#Move Start Button to the left 
+Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name TaskbarAl -Value 0 -Type Dword -Force
+
