f3ff98f80c2bbc803a3d382f4aadb1a91f69f3e1
jenssgb <jens.epost@googlemail.com>
2022-12-29 23:19:27 +0100

    Changed Script

--- configer-ui.ps1
+++ configer-ui.ps1
@@ -3,6 +3,8 @@ Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Pe
 Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name SystemUsesLightTheme -Value 0 -Type Dword -Force
 
 #Move Start Button to the left 
+Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name TaskbarAl -Value 1 -Type Dword -Force
 Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name TaskbarAl -Value 0 -Type Dword -Force
+
 
 shutdown -r -t 0
