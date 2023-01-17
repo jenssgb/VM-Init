58c24c6180af305993cd2d556ba63d315d9278f7
jenssgb <jens.epost@googlemail.com>
2023-01-17 18:39:30 +0100

    Update InstallBGInfo.ps1

--- InstallBGInfo.ps1
+++ InstallBGInfo.ps1
@@ -1,4 +1,5 @@
-
+#Download the RAR Git Script:
+#(Invoke-WebRequest -Uri "https://raw.githubusercontent.com/jenssgb/VM-Init/main/InstallBGInfo.ps1").Content | Out-File (Join-Path $PWD "InstallBGInfo.ps1")
 
 
 if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
@@ -22,3 +23,18 @@ if (Test-Path (Join-Path $output "BGInfo.exe")) {
 
 Invoke-WebRequest -Uri $url -OutFile $file
 Expand-Archive -LiteralPath $file -DestinationPath $output
+
+
+#Download my BG Info Template: 
+
+
+
+#Create the Task:
+
+$action = New-ScheduledTaskAction -Execute 'C:\Program Files\BGInfo\BgInfo64.exe' -Argument 'C:\Program Files\BGInfo\JensBgInfoConfig.bgi /timer:0'
+$trigger1 = New-ScheduledTaskTrigger -AtLogOn
+$trigger2 = New-ScheduledTaskTrigger -SessionStateChange -State "SessionConnected"
+$principal = New-ScheduledTaskPrincipal -UserId "SYSTEM" -LogonType ServiceAccount
+Register-ScheduledTask -Action $action -Trigger $trigger1,$trigger2 -TaskName "BGInfo at Logon and Session Connect" -Principal $principal
+
+
